d1dceaf346d30d82ddeb7ebcb250a1924ea4839c
Sankerp <sankerp78@gmail.com>
2023-08-15 10:53:55 +0300

    init app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>1111</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
